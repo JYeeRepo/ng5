ce6ad871f45b78d294034ddcae45302977c3c428
@JYeeRepo <comic_rage@yahoo.com>
2018-09-22 13:04:29 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService } from './data.service'; // import from data service to here
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // add animation 
+import { FormsModule } from '@angular/forms'; // add this for two way data binding
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService], // add new service here
   bootstrap: [AppComponent]
 })
 export class AppModule { }
